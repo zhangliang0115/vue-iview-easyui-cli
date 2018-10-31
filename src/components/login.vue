9bf1c8305b6644104663df565bf108ceeabf4670
张良 <583564331@qq.com>
2018-10-31 16:58:29 +0800

    11

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,21 +1,18 @@
 <template>
-  <div id="app">
-      <Alert type="success">A success prompt</Alert>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App4"/>
-	<MyEasyUI > </MyEasyUI>
-  </div>
+    <div>
+        <DataGrid :data="data" style="height:250px">
+            <GridColumn field="itemid" title="Item ID"></GridColumn>
+            <GridColumn field="name" title="Name"></GridColumn>
+            <GridColumn field="listprice" title="List Price" align="right"></GridColumn>
+            <GridColumn field="unitcost" title="Unit Cost" align="right"></GridColumn>
+            <GridColumn field="attr" title="Attribute" width="30%"></GridColumn>
+            <GridColumn field="status" title="Status" align="center"></GridColumn>
+        </DataGrid>
+    </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import MyEasyUI from './components/MyEasyUI.vue'
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,MyEasyUI
-  },
-  data() {
+    export default {
+        data() {
             return {
                 data: [
                     {"code":"FI-SW-01","name":"Koi","unitcost":10.00,"status":"P","listprice":36.50,"attr":"Large","itemid":"EST-1"},
@@ -31,16 +28,5 @@ export default {
                 ]
             }
         }
-}
+    }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import 'vx-easyui/dist/themes/default/easyui.css';
+import 'vx-easyui/dist/themes/icon.css';
+import 'vx-easyui/dist/themes/vue.css';
+import EasyUI from 'vx-easyui';
+
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+
+Vue.use(EasyUI);
+Vue.use(iView);
 
 Vue.config.productionTip = false
 
